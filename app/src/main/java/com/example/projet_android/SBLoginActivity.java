73cfbc10ae6083f97d5a8defd776019c319ab47f
RomainReina <romain.reina98@gmail.com>
2021-02-11 11:57:24 +0100

    Add Logo & Debut LoginActivity

---
+++ app/src/main/java/com/example/projet_android/SBLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.projet_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SBLoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login_layout);
+    }
+}
