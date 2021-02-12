d54f8d78e637b698ab31aa6376b2fee9954da82f
Blasselle <thib.blasselle@gmail.com>
2021-02-12 14:52:25 +0100

    Création Home page + Menu

--- app/src/main/java/com/example/projet_android/MainActivity.java
+++ app/src/main/java/com/example/projet_android/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.projet_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     //Coucou les copains
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_home_page);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.option1:
+
+                startActivity(new Intent(this, MainActivity.class));
+
+                return true;
+
+            case R.id.Settings:
+
+                startActivity(new Intent(this, MainActivity.class));
+
+                return true;
+
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
